ea4e61a2640d935a92984dcd8467ad7943d4206c
Pankaj Patel <patel.pankaj@live.in>
2017-09-03 18:38:08 +0000

    firebase added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./rpi.log");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://rpi-ping-pong.firebaseio.com"
+});
+
+
+
 var Gpio = require('pigpio').Gpio,
   button = new Gpio(4, {
     mode: Gpio.INPUT,
